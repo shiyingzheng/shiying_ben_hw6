53e4c65a71b04f7bf2bee0c2d5acdfad2f670090
BEN <bstern+github@oberlin.edu>
2013-10-28 22:38:51 -0500

    added functions to format.c

--- format.c
+++ format.c
@@ -11,14 +11,45 @@
 /*
  *returns true if c points to an empty line, false otherwise
  */
-int isEmptyLine(char* s){
+int is_empty_line(){
 	//TODO not implemented yet
 	return 0;
 }
 /*
  * Reads input from standardIn and returns the next paragraph
  */
-char* nextParagraph(){
+char* next_paragraph(){
+	//TODO not implemented yet
+}
+/*
+ * returns a pointer to the next word
+ */
+char* next_word(char* c){
+	//TODO not implemented yet
+}
+/*
+ * format one paragraph left aligned
+ */
+char* format_left_align(char* par, int width){
+	//TODO not implemented yet
+}
+/*
+ * format one paragraph right aligned
+ */
+char* format_right_align(char* par, int width){
+        //TODO not implemented yet
+}
+/*
+ * format one paragraph justified
+ */
+char* format_justified(char* par, int width){
+        //TODO not implemented yet
+}
+
+/*
+ * format one paragraph in the correct mode
+ */
+char* format_paragraph(char* par,char mode, int width){
 	//TODO not implemented yet
 }
 int main(){
